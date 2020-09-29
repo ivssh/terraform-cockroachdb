b0a2bf9a0a50f447c2d0845025538b7258689b90
Abhilash Bolla <abhilash.gis@gmail.com>
2020-09-30 01:26:08 +0530

    mild refactoring w.r.t security groups

---
+++ security_groups.tf
@@ -0,0 +1,53 @@
+resource "aws_security_group" "roach_sg" {
+  name   = "${terraform.workspace} Roach Security Group"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8008
+    to_port     = 8080
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 26257
+    to_port     = 26257
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name   = "${terraform.workspace} Bastion Security Group"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
